fbb1cc262139d5ed66f898a41716b7a25e64223e
abdullah-dar <darabdullah328@gmail.com>
2021-06-22 12:41:32 +0500

    done

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("mongodb+srv://admin:admin@cluster0.tnvbh.mongodb.net/Cluster0", {
+  .connect("mongodb+srv://admin:admin@cluster0.tnvbh.mongodb.net/Terminal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
